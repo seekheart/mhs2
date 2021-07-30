6313d8278cf0462004af03cee661112300779b5b
seekheart <miketung2013@gmail.com>
2021-07-30 14:39:08 -0400

    save pt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: "monsters", redirectTo: "/monsters", pathMatch: "full"
+  },
+  {
     path: "", redirectTo: "/monsters", pathMatch: "full"
+  },
+  {
+    path: "**", redirectTo: "/monsters", pathMatch: "full"
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
